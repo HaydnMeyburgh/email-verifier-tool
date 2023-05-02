e61cc5c8f98545a89bfd26e8782b17e36673c603
Haydn <haydnmey@gmail.com>
2023-05-02 09:02:57 +0200

    used LookupMX function to see if there is an mx of the domain

--- main.go
+++ main.go
@@ -27,5 +27,16 @@ func checkDomain(domain string) {
 	var hasMX, hasSPF, hasDMARC bool
 	var spfRecord, dmarcRecord string
 
+	// Looking up the mx of the domain
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
 	
 }
